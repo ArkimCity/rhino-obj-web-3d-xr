d221ef77d2780120662d54850c94e8c7721f699b
Jaeung Kim <jaeung949@gmail.com>
2021-08-22 14:52:18 +0900

    test

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -4,4 +4,7 @@ module.exports = {
   outputDir: path.resolve(__dirname, "../docs"),
   // publicPath: './rhino-and-obj-to-web3d-xr/',
   // assetsDir: "./rhino-and-obj-to-web3d-xr/"
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/rhino-and-obj-to-web3d-xr/'
+    : '/'
 }
